31d6780d33b7775936b766afe09a715e5cdc4e8f
User McName <anna@thisvery.space>
2017-08-10 16:26:13 +0000

    Added notice and styles.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
    
-   def create
+   before_filter :select_plan, only: :new
+   
+    def create
         super do |resource|
             if params[:plan]
                 resource.plan_id = params[:plan]
@@ -13,5 +15,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
         end    
     end
    
-    
+    private
+        def select_plan
+            unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+                flash[:notice] = "Please select a membership plan to sign up."
+                redirect_to root_url
+            end
+        end
 end
